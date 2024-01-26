6772ec30e1bccf8e2043f13ebe2de02b6ab61e08
Taketo Takashima <t.taketo1113@gmail.com>
2024-01-27 00:46:26 +0900

    Added spec for resolver of do53

---
+++ spec/ddig/resolver/do53_spec.rb
@@ -0,0 +1,141 @@
+# frozen_string_literal: true
+
+RSpec.describe Ddig::Resolver::Do53 do
+  context "#lookup" do
+    before(:each) do
+      @do53 = Ddig::Resolver::Do53.new(hostname: 'dns.google')
+      @do53.lookup
+    end
+
+    it "a/aaaa return values" do
+      # a
+      expect(@do53.a).to include "8.8.8.8"
+      expect(@do53.a).to include "8.8.4.4"
+
+      # aaaa
+      expect(@do53.aaaa).to include "2001:4860:4860::8844"
+      expect(@do53.aaaa).to include "2001:4860:4860::8888"
+    end
+
+    it "hostname set value" do
+      expect(@do53.hostname).to eq 'dns.google'
+    end
+
+    it "nameservers set values" do
+      expect(@do53.nameservers).not_to eq nil
+    end
+
+    it "ip is nil" do
+      expect(@do53.ip).to eq nil
+    end
+  end
+
+  context "#lookup: with nameserver" do
+    before(:each) do
+      @nameservers = ['8.8.8.8']
+
+      @do53 = Ddig::Resolver::Do53.new(hostname: 'dns.google', nameservers: @nameservers)
+      @do53.lookup
+    end
+
+    it "a/aaaa return values" do
+      # a
+      expect(@do53.a).to include "8.8.8.8"
+      expect(@do53.a).to include "8.8.4.4"
+
+      # aaaa
+      expect(@do53.aaaa).to include "2001:4860:4860::8844"
+      expect(@do53.aaaa).to include "2001:4860:4860::8888"
+    end
+
+    it "hostname set value" do
+      expect(@do53.hostname).to eq 'dns.google'
+    end
+
+    it "nameservers set values" do
+      expect(@do53.nameservers).to eq ['8.8.8.8']
+    end
+
+    it "ip is nil" do
+      expect(@do53.ip).to eq nil
+    end
+  end
+
+  context "#lookup: with invalid nameserver" do
+    before(:each) do
+      @nameservers = ['invalid']
+
+      @do53 = Ddig::Resolver::Do53.new(hostname: 'dns.google', nameservers: @nameservers)
+    end
+
+    it "raise Socket::ResolutionError" do
+      expect {
+        @do53.lookup
+      }.to raise_error(Socket::ResolutionError)
+    end
+  end
+
+  context "#lookup: ipv4" do
+    before(:each) do
+      @ip = :ipv4
+      @nameservers = ['8.8.8.8', '2001:4860:4860::8888']
+
+      @do53 = Ddig::Resolver::Do53.new(hostname: 'dns.google', ip: @ip, nameservers: @nameservers)
+      @do53.lookup
+    end
+
+    it "a/aaaa return values" do
+      # a
+      expect(@do53.a).to include "8.8.8.8"
+      expect(@do53.a).to include "8.8.4.4"
+
+      # aaaa
+      expect(@do53.aaaa).to include "2001:4860:4860::8844"
+      expect(@do53.aaaa).to include "2001:4860:4860::8888"
+    end
+
+    it "hostname set value" do
+      expect(@do53.hostname).to eq 'dns.google'
+    end
+
+    it "nameservers set values" do
+      expect(@do53.nameservers).to eq ['8.8.8.8']
+    end
+
+    it "ip is nil" do
+      expect(@do53.ip).to eq :ipv4
+    end
+  end
+
+  context "#lookup: ipv6" do
+    before(:each) do
+      @ip = :ipv6
+      @nameservers = ['8.8.8.8', '2001:4860:4860::8888']
+
+      @do53 = Ddig::Resolver::Do53.new(hostname: 'dns.google', ip: @ip, nameservers: @nameservers)
+      @do53.lookup
+    end
+
+    it "a/aaaa return values" do
+      # a
+      expect(@do53.a).to include "8.8.8.8"
+      expect(@do53.a).to include "8.8.4.4"
+
+      # aaaa
+      expect(@do53.aaaa).to include "2001:4860:4860::8844"
+      expect(@do53.aaaa).to include "2001:4860:4860::8888"
+    end
+
+    it "hostname set value" do
+      expect(@do53.hostname).to eq 'dns.google'
+    end
+
+    it "nameservers set values" do
+      expect(@do53.nameservers).to eq ['2001:4860:4860::8888']
+    end
+
+    it "ip is nil" do
+      expect(@do53.ip).to eq :ipv6
+    end
+  end
+end
--- spec/ddig_spec.rb
+++ spec/ddig_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe Ddig do
   it "has a version number" do
     expect(Ddig::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(false)
-  end
 end

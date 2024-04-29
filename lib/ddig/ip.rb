7f8601f2c7b64b93a2ec7b05df0969e24dc56abc
Taketo Takashima <t.taketo1113@gmail.com>
2024-04-29 15:52:39 +0900

    Added Ddig::Ip#ip_type

--- lib/ddig/ip.rb
+++ lib/ddig/ip.rb
@@ -2,6 +2,33 @@
 
 module Ddig
   class Ip
+    attr_reader :ip_type
+
+    def initialize(use_ipv4: nil, use_ipv6: nil)
+      @use_ipv4 = use_ipv4
+      @use_ipv6 = use_ipv6
+
+      set_ip_type
+    end
+
+    def set_ip_type
+      if @use_ipv4.nil? && self.class.enable_ipv4?
+        @use_ipv4 = true
+      end
+
+      if @use_ipv6.nil? && self.class.enable_ipv6?
+        @use_ipv6 = true
+      end
+
+      if @use_ipv4 && @use_ipv6
+        @ip_type = :all
+      elsif @use_ipv4
+        @ip_type = :ipv4
+      elsif @use_ipv6
+        @ip_type = :ipv6
+      end
+    end
+
     def self.enable_ipv4?
       ip_list.any? { |addr| addr.ipv4? }
     end
--- spec/ddig/ip_spec.rb
+++ spec/ddig/ip_spec.rb
@@ -10,4 +10,46 @@ RSpec.describe Ddig::Ip do
     skip "IPv6 enabled: #{Ddig::Ip.enable_ipv6?}" unless enable_ipv6?
     expect(Ddig::Ip.enable_ipv6?).to be true
   end
+
+  context "ip_type" do
+    it "return :all with use_ipv4 & use_ipv6 is true" do
+      use_ipv4 = true
+      use_ipv6 = true
+
+      expect(Ddig::Ip.new(use_ipv4: use_ipv4, use_ipv6: use_ipv6).ip_type).to be :all
+    end
+
+    it "return :ipv4 with use_ipv4: true & use_ipv6: false" do
+      use_ipv4 = true
+      use_ipv6 = false
+
+      expect(Ddig::Ip.new(use_ipv4: use_ipv4, use_ipv6: use_ipv6).ip_type).to be :ipv4
+    end
+
+    it "return :all with use_ipv4: false & use_ipv6: true" do
+      use_ipv4 = false
+      use_ipv6 = true
+
+      expect(Ddig::Ip.new(use_ipv4: use_ipv4, use_ipv6: use_ipv6).ip_type).to be :ipv6
+    end
+
+    context "use_ipv4/use_ipv6: nil" do
+      before(:each) do
+        @use_ipv4 = nil
+        @use_ipv6 = nil
+      end
+
+      it "return ip_type via enable_ipv4?/enable_ipv6?" do
+        if enable_ipv4? && enable_ipv6?
+          ip_type = :all
+        elsif enable_ipv4?
+          ip_type = :ipv4
+        elsif enable_ipv6?
+          ip_type = :ipv6
+        end
+
+        expect(Ddig::Ip.new(use_ipv4: @use_ipv4, use_ipv6: @use_ipv6).ip_type).to be ip_type
+      end
+    end
+  end
 end
